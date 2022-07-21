dacacb57016fc59da9d5db85597a91d28e7a1f68
Prathap <prathap.sims@gmail.com>
2022-07-21 23:42:17 +0000

    add creating the tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,58 +3,65 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+$(document).ready (() => {
 
-// const tweets = [
-//       {
-//          user: {
-//           name: "Newton",
-//           avatars: "https://i.imgur.com/73hZDYK.png",
-//           handle: "@SirIsaac",
-//         },
-//         content: {
-//           text: "If I have seen further it is by standing on the shoulders of giants",
-//         },
-//         created_at: 1461116232227,
-//       }
-//       {
-//         user: {
-//           name: "Descartes",
-//           avatars: "https://i.imgur.com/nlhLi3I.png",
-//           handle: "@rd",
-//         },
-//         content: {
-//           text: "Je pense , donc je suis",
-//         },
-//         created_at: 1461113959088,
-//       },
-      
-//     ];
+const database = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+    ]
 
-const tweetData = {
-  user: {
-   name: "Newton",
-   avatars: "https://i.imgur.com/73hZDYK.png",
-   handle: "@SirIsaac",
- },
- content: {
-   text: "If I have seen further it is by standing on the shoulders of giants",
- },
- created_at: 1461116232227,
-};
+    const renderTweets = function (tweets) {
+      tweets.forEach((tweet) => {
+        const newTweet = createTweetElement(tweet);
+        $('.tweets-container').append(newTweet);
+      });
+    };
 
-const createTweetElement = function (tweets) {
+// const tweetData = {
+//   user: {
+//    name: "Newton",
+//    avatars: "https://i.imgur.com/73hZDYK.png",
+//    handle: "@SirIsaac",
+//  },
+//  content: {
+//    text: "If I have seen further it is by standing on the shoulders of giants",
+//  },
+//  created_at: 1461116232227,
+// };
+
+const createTweetElement = function (tweet) {
   const $tweetTemplate = `
   <article class="tweet">
     <header>
-    <img src="${tweets.user.avatars}">
-      <div><span>${tweets.user.name}</span></div>
-      <div>${tweets.user.handle}</div>
+    <img src="${tweet.user.avatars}">
+      <div><span>${tweet.user.name}</span></div>
+      <div>${tweet.user.handle}</div>
     </header>
 
-    <p> ${tweets.content.text} </p>
+    <p> ${tweet.content.text} </p>
     
     <footer>
-      <span>${tweets.created_at} </span>
+      <span>${tweet.created_at} </span>
       <div>
         <a href="#"><i class="fa-solid fa-flag"></i></a> 
         <a href="#"><i class="fa-solid fa-retweet"></i></a> 
@@ -63,23 +70,16 @@ const createTweetElement = function (tweets) {
     </footer>
   </article>
   `;
-
   return $tweetTemplate;
 };
 
-const $tweet = createTweetElement(tweetData);
+// const $tweet = createTweetElement(tweets);
 
 // Test / driver code (temporary)
 //console.log($tweet); // to see what it looks like
-$(document).ready (() => {
-  console.log($tweet);
-$('.tweets-container').append($tweet)});
+// $(document).ready (() => {
+//   console.log($tweet);
+// $('.tweets-container').append($tweet)})
 
-  // const renderTweets = function (tweets) {
-  //   tweets.forEach((tweet) => {
-  //     const newTweet = createTweetElement(tweet);
-  //     $('#tweet-container').append(newTweet);
-  //   });
-  // };
-
-   //renderTweets(tweets);
+   renderTweets(database);
+});
