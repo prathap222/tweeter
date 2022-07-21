6b34a13c5da030c2206efc7903892bf27412486b
Prathap <prathap.sims@gmail.com>
2022-07-21 22:50:28 +0000

    add modify client.js

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,29 +5,30 @@
  */
 
 // const tweets = [
-      // {
-      //    user: {
-      //     name: "Newton",
-      //     avatars: "https://i.imgur.com/73hZDYK.png",
-      //     handle: "@SirIsaac",
-      //   },
-      //   content: {
-      //     text: "If I have seen further it is by standing on the shoulders of giants",
-      //   },
-      //   created_at: 1461116232227,
-      // }
-      // {
-      //   user: {
-      //     name: "Descartes",
-      //     avatars: "https://i.imgur.com/nlhLi3I.png",
-      //     handle: "@rd",
-      //   },
-      //   content: {
-      //     text: "Je pense , donc je suis",
-      //   },
-      //   created_at: 1461113959088,
-      // },
-    // ];
+//       {
+//          user: {
+//           name: "Newton",
+//           avatars: "https://i.imgur.com/73hZDYK.png",
+//           handle: "@SirIsaac",
+//         },
+//         content: {
+//           text: "If I have seen further it is by standing on the shoulders of giants",
+//         },
+//         created_at: 1461116232227,
+//       }
+//       {
+//         user: {
+//           name: "Descartes",
+//           avatars: "https://i.imgur.com/nlhLi3I.png",
+//           handle: "@rd",
+//         },
+//         content: {
+//           text: "Je pense , donc je suis",
+//         },
+//         created_at: 1461113959088,
+//       },
+      
+//     ];
 
 const tweetData = {
   user: {
@@ -69,9 +70,10 @@ const createTweetElement = function (tweets) {
 const $tweet = createTweetElement(tweetData);
 
 // Test / driver code (temporary)
-console.log($tweet); // to see what it looks like
+//console.log($tweet); // to see what it looks like
 $(document).ready (() => {
-('.tweets-container').append($tweet)});
+  console.log($tweet);
+$('.tweets-container').append($tweet)});
 
   // const renderTweets = function (tweets) {
   //   tweets.forEach((tweet) => {
@@ -80,4 +82,4 @@ $(document).ready (() => {
   //   });
   // };
 
-  // renderTweets(tweets);
+   //renderTweets(tweets);
