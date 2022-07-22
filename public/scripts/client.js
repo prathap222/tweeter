447d8ef298896ac29303d48be13eb7ebd053b6f8
Prathap <prathap.sims@gmail.com>
2022-07-22 03:22:56 +0000

    add fetch tweet methods using ajax

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,74 +3,6 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-$(document).ready (() => {
-
-const database = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-    ]
-
-    const renderTweets = function (tweets) {
-      tweets.forEach((tweet) => {
-        const newTweet = createTweetElement(tweet);
-        $('.tweets-container').append(newTweet);
-      });
-    };
-
-    $('.input form').submit(function(event){
-      event.preventDefault();
-    })
-
-    // serialize the data from form input and send to server
-
-    $.ajax('/tweets', {
-      data: $(this).serialize(),
-      method: 'POST'
-    });
-
-    // Get tweets from server
-
-    const loadTweets = () => {
-      $.ajax('/tweets', {
-        method: 'GET',
-        dataType: 'JSON'
-      })
-        .then(tweets => renderTweets(tweets));
-    };
-  
-    loadTweets();
-
-// const tweetData = {
-//   user: {
-//    name: "Newton",
-//    avatars: "https://i.imgur.com/73hZDYK.png",
-//    handle: "@SirIsaac",
-//  },
-//  content: {
-//    text: "If I have seen further it is by standing on the shoulders of giants",
-//  },
-//  created_at: 1461116232227,
-// };
 
 const createTweetElement = function (tweet) {
   const $tweetTemplate = `
@@ -84,7 +16,7 @@ const createTweetElement = function (tweet) {
     <p> ${tweet.content.text} </p>
     
     <footer>
-      <span>${tweet.created_at} </span>
+      <span>${timeago.format(tweet.created_at)} </span>
       <div>
         <a href="#"><i class="fa-solid fa-flag"></i></a> 
         <a href="#"><i class="fa-solid fa-retweet"></i></a> 
@@ -96,13 +28,25 @@ const createTweetElement = function (tweet) {
   return $tweetTemplate;
 };
 
-// const $tweet = createTweetElement(tweets);
+const renderTweets = function (tweets) {
+      
+  for (const tweet of tweets) {
+      const newTweet = createTweetElement(tweet);
+      $('.tweets-container').prepend(newTweet);
+    }
+  };
 
-// Test / driver code (temporary)
-//console.log($tweet); // to see what it looks like
-// $(document).ready (() => {
-//   console.log($tweet);
-// $('.tweets-container').append($tweet)})
 
-   renderTweets(database);
-});
+$(document).ready (() => {
+
+  // Get tweets from server
+  const loadTweets = () => {
+    $.ajax('/tweets', {
+      method: 'GET',
+      dataType: 'JSON'
+    }) .then(tweets => renderTweets(tweets));
+  };
+
+  loadTweets();
+
+})    
